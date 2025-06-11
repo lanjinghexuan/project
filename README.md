692000242b3a891f64d1fb22f960446613f2eb2e
吕岩 <1258321826@qq.com>
2025-06-11 21:26:48 +0800

    jwt简易版

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # project
 这是一个测试的项目个人使用
 
+## elasticsearch
 pkg目前封装了es的操作
 具体使用方式可以参考 "common/cmd/createEs/main.go"
 
+## 通义千问
 目前项目中还有关于阿里云通义千问的调用
 代码地址为 "api/handle/Alichatgpt.go"
 路由地址为 "api/main.go“
 关于流式输出的文件会在 "api/files" 
 
+## mongodb
 文档编写了go操作mongodb的增删查改
 代码地址为 "common/cmd/mongo/main.go"
+
+## jwt
+文档有一个简易的jwt生成token
+代码地址为 "common/pkr/jwt.go"
--- common/pkr/jwt.go
+++ common/pkr/jwt.go
@@ -1,37 +1,59 @@
 package pkr
 
 import (
+	"errors"
 	"fmt"
 	"github.com/golang-jwt/jwt/v5"
+	"log"
+	"time"
 )
 
-// 生成JWT
-func GenerateJWT(username int32) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"Id": username,
-	})
-	jwtkey := make([]byte, 32)
-	// Sign and get the complete encoded token as a string using the secret
-	tokenString, err := token.SignedString(jwtkey)
+//代码从官方查询个人参考使用
 
-	return tokenString, err
+type MyCustomClaims struct {
+	Foo string `json:"foo"`
+	jwt.RegisteredClaims
 }
 
-// 解析JWT
-func ParseJwt(tokenstring string) (map[string]interface{}, error) {
-	token, err := jwt.Parse(tokenstring, func(token *jwt.Token) (interface{}, error) {
-		return []byte("AllYourBase"), nil
-	})
+func GetToken(UserId string) (string, error) {
+	//密钥
+	mySigningKey := []byte("AllYourBase")
 
+	// Create claims with multiple fields populated
+	claims := MyCustomClaims{
+		"bar",
+		jwt.RegisteredClaims{
+			// A usual scenario is to set the expiration time relative to the current time
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)), //过期时间
+			IssuedAt:  jwt.NewNumericDate(time.Now()),                     //发布时间
+			NotBefore: jwt.NewNumericDate(time.Now()),                     //生效时间
+			ID:        UserId,                                             //内容
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err := token.SignedString(mySigningKey)
+	fmt.Println(ss, err)
+	return ss, nil
+}
+
+func ParseToken(tokenString string) (string, error) {
+
+	type MyCustomClaims struct {
+		Foo string `json:"foo"`
+		jwt.RegisteredClaims
+	}
+
+	token, err := jwt.ParseWithClaims(tokenString, &MyCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte("AllYourBase"), nil
+	}, jwt.WithLeeway(time.Second))
 	if err != nil {
+		fmt.Println(err)
+	} else if claims, ok := token.Claims.(*MyCustomClaims); ok {
+		return claims.ID, nil
+	} else {
+		log.Fatal("unknown claims type, cannot proceed")
 	}
-	if token.Valid {
-		fmt.Println("Token is invalid")
-	}
-	tokenClaims, ok := token.Claims.(jwt.MapClaims)
-	if !ok {
-		fmt.Println("Token is invalid;line 33")
-	}
-	
-	return tokenClaims, nil
+	return "", errors.New("unknown claims type, cannot proceed")
+	// Output: bar test
 }
